7f13b4b49d63017d1fad6ea4623d36bbce6b82a0
Koalazz <yaxuanhe@gmail.com>
2014-03-06 10:22:42 +0100

    Simple Scene

--- 2d_game_gourp3/Assets/Script/Player.cs
+++ 2d_game_gourp3/Assets/Script/Player.cs
@@ -3,6 +3,13 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
+	public float moveForce = 365f;
+	public float maxSpeed = 5f;
+	public float jumpForce = 10f;
+	public bool jump = false;
+	public bool facingRight = true;
+
+	private bool grounded = true;
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +17,32 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(Input.GetButtonDown("Jump") && grounded)
+			jump = true;
+	}
+
+	void FixedUpdate()
+	{
+		float hrizontal = Input.GetAxis ("Horizontal");
+
+		if (hrizontal * rigidbody2D.velocity.x < maxSpeed)
+						rigidbody2D.AddForce (Vector2.right * hrizontal * moveForce);
+		if (Mathf.Abs(rigidbody2D.velocity.x) > maxSpeed)
+			rigidbody2D.velocity = new Vector2(Mathf.Sign(rigidbody2D.velocity.x) * maxSpeed, rigidbody2D.velocity.y);
+		if (hrizontal > 0 && !facingRight)
+						Flip ();
+				else if (hrizontal < 0 && facingRight)
+						Flip ();
+		if (jump) 
+		{
+			rigidbody2D.AddForce(new Vector2(0f, jumpForce));
+
+			jump = false;
+		}
+	}
+
+	void Flip()
+	{
+
 	}
 }
