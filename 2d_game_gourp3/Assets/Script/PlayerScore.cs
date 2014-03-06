00e05b312c7c1509c57caaed604cf2af4ca66e9d
Tacx <cryszone@gmail.com>
2014-03-06 18:34:15 +0100

    Created Player Score

---
+++ 2d_game_gourp3/Assets/Script/PlayerScore.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScore : MonoBehaviour {
+
+	public int score = 0;
+	
+	
+	private Player player;
+	private int previousScore = 0;
+	private int levelScore = 1000;
+
+	void Awake()
+	{
+		player = GameObject.FindGameObjectWithTag ("Player").GetComponent<Player>();
+	}
+
+	void Update () {
+
+		//guiText.text = "Score: " + score;
+		if(previousScore != score)
+		previousScore = score;
+	}
+
+	// This will be probably the part for the faster spawn of mobs
+	void FixedUpdate()
+	{
+		if (score != 0 && score % 1000 == 0) 
+		{
+			// increase the spawn and number of mobs
+			// method will be implemented here :)
+		}
+	}
+
+	void OnGUI() {
+		GUI.Box (new Rect (1200,72,100,30), "Score: " + score);
+	}
+}
