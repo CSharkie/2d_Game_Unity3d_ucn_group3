df8eeb821e3246b25401256fd427897b07d2761e
Tacx <cryszone@gmail.com>
2014-03-19 11:18:23 +0100

    Added Start Menu - needs wrokout for screen size Mods in the Scenes and Build options

---
+++ 2d_game_gourp3/Assets/Script/StartScreen.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI () 
+	{
+		GUILayout.BeginArea (new Rect (200, 200, 300, 400));
+		GUILayout.Label ("Welcome to the Game !");
+		if( GUILayout.Button ("Start"))
+			Application.LoadLevel("BG");
+		if (GUILayout.Button ("Exit"))
+			Application.Quit();
+
+		GUILayout.EndArea ();
+	}
+}
