c001789559635e20338020b08e1de9ed56b22cd2
Tacx <cryszone@gmail.com>
2014-03-23 13:32:09 +0100

    Implemented pause for the game and partial score need death for mobs to finish.

---
+++ 2d_game_gourp3/Assets/Script/PauseMenu.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI () 
+	{
+		GUILayout.BeginArea (new Rect (Screen.width/2-150, Screen.height/2-50, 300, 400));
+		GUILayout.Label ("Game Paused !");
+		if( GUILayout.Button ("Resume"))
+			Destroy(this);
+		if (GUILayout.Button ("Exit"))
+			Application.Quit();
+		
+		GUILayout.EndArea ();
+	}
+}
